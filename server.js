d5fe5760893e94fbda95489e5f6d042ad95753b3
Matteo <matteo.vettosi@trustonic.com>
2020-05-13 22:29:07 +0100

    added reading port from env

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 var http = require("http");
+var port = normalizePort(process.env.PORT || '4000');
 
 http.createServer(function (request, response) {
-   // Send the HTTP header 
+   // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   
+
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(port);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:' + port);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
